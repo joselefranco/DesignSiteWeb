6aa671ed8a69f4519229f2edbde8c5bfdb208f1f
Thomas Chekaiban <thomaschekaiban@gmail.com>
2017-06-20 13:41:52 +0200

    12th commit - storing on the database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 var exphbs  = require('express-handlebars');
-
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://essec:cergyisc00l@138.68.110.210:27017/admin?readPreference=primary';
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -9,10 +10,22 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('static'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/contact",function(req, res){
+// parse application/json
+app.use(bodyParser.json());
 
-  res.send(200);
+
+MongoClient.connect(url, function(err, db) {
+
+  var collection = db.collection('contactrock');
+
+  app.post("/contact",function(req, res){
+    collection.insert(req.body,function(err,result){
+            res.send(200);
+
+        });
+  });
 });
 
 app.listen(9999, function () {
